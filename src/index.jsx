b27d9e5a9c5277090628ede56b3e9fe85f0679a4
Denis Cheung <d.s.h.cheung@gmail.com>
2021-06-27 16:41:28 +0800

    hot reload

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,22 @@
-import 'react-hot-loader'
+import { AppContainer } from 'react-hot-loader'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from '@/layouts/App'
 import '@/styles/index.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const render = () => {
+  ReactDOM.render((
+    <AppContainer>
+      <App />
+    </AppContainer>
+  ), document.getElementById('root'))
+}
+
+render()
+
+if (module.hot) {
+  module.hot.accept('@/layouts/App', () => {
+    render(App)
+  })
+}
--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
-import { hot } from 'react-hot-loader'
 
 import PagesHome from '@/pages/Home'
 import PagesAnother from '@/pages/Another'
@@ -16,4 +15,4 @@ const App = () => (
   </Router>
 )
 
-export default hot(module)(App)
+export default App
